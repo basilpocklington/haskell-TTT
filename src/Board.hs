9904fd20c105f689f526d9b35ed5d89624fc33ba
basil <bjorn.johnson35@gmail.com>
2016-06-27 18:01:22 -0400

    Add board updating

--- src/Board.hs
+++ src/Board.hs
@@ -1,15 +1,24 @@
 module Board 
   ( Symbol 
   , empty
+  , x
   , newBoard
+  , updateBoard
   ) where
 
-data Symbol = E deriving (Eq)
+data Symbol = E
+            | X
+            deriving (Eq)
 
 empty = E
+x = X
 
 newBoard :: Int -> [Symbol] 
 newBoard size = take (size * size) (repeat empty) 
 
+updateBoard :: Int -> Symbol -> [Symbol] -> [Symbol]
+updateBoard space symbol board = take (space - 1) board ++ [symbol] ++ drop space board
+
 instance Show Symbol where
   show E = show '_'
+  show X = show 'x'
--- test/BoardTestSpec.hs
+++ test/BoardTestSpec.hs
@@ -10,8 +10,8 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "Board Tests" $ do
-    it "should return new 9 space empty board" $ do 
+    it "should return new 9 space empty board" $ do
       (newBoard 3) `shouldBe` (take 9 (repeat empty))
 
-
-
+    it "should update board when symbol is placed" $ do
+      (updateBoard 1 x (take 9 (repeat empty))) `shouldBe` x:(take 8 (repeat empty))
